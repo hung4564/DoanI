5865ec6352650a3f6f64e41bd4e5705ea55af22c
Hung <hphamviet83@gmail.com>
2018-05-07 19:26:32 +0700

    khoi tao

---
+++ app/User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'is_admin', 'logo_number',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    /**
+     * @return boolean
+     */
+    public function isAdmin()
+    {
+        return (int) $this->is_admin === 1;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogoPath()
+    {
+        return Utils::logoPath($this->logo_number);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRecordTitle()
+    {
+        return $this->name;
+    }
+}
