14a306e2fbf8caec62cc1aa669d922cb89d21feb
Hung <hphamviet83@gmail.com>
2018-05-08 08:38:10 +0700

    them cac tien ich va thu vien can thiet

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,58 +1,80 @@
-{{-- Extends Layout --}}
-@extends('layouts.backend')
-
-{{-- Page Title --}}
-@section('page-title', 'Admin')
-
-{{-- Page Subtitle --}}
-@section('page-subtitle', 'Control panel')
-
-{{-- Breadcrumbs --}}
+{{-- Extends Layout --}} 
+@extends('layouts.backend') {{-- Page Title --}} 
+@section('page-title', 'Admin') {{-- Page Subtitle
+--}} 
+@section('page-subtitle', 'Control panel') {{-- Breadcrumbs --}} 
 @section('breadcrumbs')
 @endsection
-
-{{-- Header Extras to be Included --}}
-@section('head-extras')
-    @parent
+ {{-- Header Extras
+to be Included --}} 
+@section('head-extras') @parent
 @endsection
-
+ 
 @section('content')
 
-    <!-- Small boxes (Stat box) -->
-    <div class="row">
-      <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-aqua">
-              <div class="inner">
-                  <h3>{{ $reports->getTotalUsers() }}</h3>
-                  <p>Users</p>
-              </div>
-              <div class="icon">
-                  <i class="ion ion-ios-people-outline"></i>
-              </div>
-              <a href="{{ route('admin::users.index') }}" class="small-box-footer">List Users <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
+<!-- Small boxes (Stat box) -->
+<div class="row">
+  <div class="col-lg-3 col-xs-6">
+    <!-- small box -->
+    <div class="small-box bg-aqua">
+      <div class="inner">
+        <h3>{{ $reports->getTotalUsers() }}</h3>
+        <p>Users</p>
       </div>
-      <!-- ./col -->
-     
-      <div class="col-lg-3 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-green">
-          <div class="inner">
-            <h3>{{ $reports->getTotalCategory()}}</h3>
+      <div class="icon">
+        <i class="ion ion-ios-people-outline"></i>
+      </div>
+      <a href="{{ route('admin::users.index') }}" class="small-box-footer">List Users <i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+  </div>
+  <!-- ./col -->
 
-            <p>Categories</p>
-          </div>
-          <div class="icon">
-            <i class="ion ion-stats-bars"></i>
-          </div>
-          <a href="{{ route('admin::categories.index') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
+  <div class="col-lg-3 col-xs-6">
+    <!-- small box -->
+    <div class="small-box bg-green">
+      <div class="inner">
+        <h3>{{ $reports->getTotalCategory()}}</h3>
+
+        <p>Category</p>
       </div>
-      <!-- ./col -->
+      <div class="icon">
+        <i class="ion ion-stats-bars"></i>
+      </div>
+      <a href="{{ route('admin::categories.index') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+  </div>
+  <!-- ./col -->
+  <div class="col-lg-3 col-xs-6">
+    <!-- small box -->
+    <div class="small-box bg-yellow">
+      <div class="inner">
+        <h3>44</h3>
+
+        <p>User Registrations</p>
+      </div>
+      <div class="icon">
+        <i class="ion ion-person-add"></i>
+      </div>
+      <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+  </div>
+  <!-- ./col -->
+  <div class="col-lg-3 col-xs-6">
+    <!-- small box -->
+    <div class="small-box bg-red">
+      <div class="inner">
+        <h3>65</h3>
+
+        <p>Unique Visitors</p>
+      </div>
+      <div class="icon">
+        <i class="ion ion-pie-graph"></i>
+      </div>
+      <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+  </div>
+  <!-- ./col -->
 @endsection
-
-{{-- Footer Extras to be Included --}}
+ {{-- Footer Extras to be Included --}} 
 @section('footer-extras')
-
 @endsection
